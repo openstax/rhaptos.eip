05e2d82b38c54f8fbadf44949a53b3f9316265ad
Roché Compaan <roche@upfrontsystems.co.za>
2011-12-22 20:23:26 +0200

    add processing instructions to all xml returned
    extend source fragment implementation to handle xpath selectors

--- rhaptos/eip/browser.py
+++ rhaptos/eip/browser.py
@@ -1,8 +1,12 @@
+import re
 from lxml import etree
 from zope.app.component.hooks import getSite
 from plone.app.textfield.interfaces import ITransformer
 from rhaptos.xmlfile.value import XMLTextValue
 from Products.Five.browser import BrowserView
+
+doctype = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 plus MathML 2.0//EN" "http://www.w3.org/Math/DTD/mathml2/xhtml-math11-f.dtd">"""
+
 
 class Eip(BrowserView):
     """ Eip
@@ -23,7 +27,31 @@ class Eip(BrowserView):
 
     def source_fragment(self):
         value = self.context.body
-        return value.raw
+        xpath = self.request.get('xpath', None)
+        namespaces = {}
+        if xpath is None or xpath == '/':
+            return value.raw_encoded
+        elif xpath.startswith('/cnx:'):
+            namespaces = {'cnx': 'http://cnx.rice.edu/cnxml'}
+
+        result = value.raw_encoded
+        tree = etree.fromstring(value.raw_encoded)
+        result = ''
+        for node in tree.xpath(xpath, namespaces=namespaces):
+            result += etree.tostring(node)
+
+        # if we don't strip namespaces from the fragment, eip loses the
+        # plot completely
+        result = etree.fromstring(result)
+        for prefix, ns in result.nsmap.items():
+            if prefix is None:
+                prefix = 'nil'
+            etree.strip_attributes(result, '{%s}%s' % (ns, prefix))
+        etree.cleanup_namespaces(result)
+        result = etree.tostring(result)
+        result = result.replace('xmlns="http://cnx.rice.edu/cnxml" ', '')
+
+        return result
 
     def eip_request(self):
         action = self.request.get('action',None)
@@ -45,7 +73,8 @@ class Eip(BrowserView):
                                    bodyfield.mimeType,
                                    bodyfield.outputMimeType)
             result = self.eip_transform(content)
-
+            result = bodyfield.processingInstruction + '\n' + \
+                     doctype + '\n' + result
         elif action=='update':
             bodyfield.xpathUpdate(content, xpath,
                 namespaces={'cnx': 'http://cnx.rice.edu/cnxml'})
@@ -53,6 +82,8 @@ class Eip(BrowserView):
                                    bodyfield.mimeType,
                                    bodyfield.outputMimeType)
             result = self.eip_transform(content)
+            result = bodyfield.processingInstruction + '\n' + \
+                     doctype + '\n' + result
         elif action=='delete' and xpath:
             bodyfield.xpathDelete(xpath,
                 namespaces={'cnx': 'http://cnx.rice.edu/cnxml'})
